22b7a2412ecd5ec0de3be9d1e1e0e91cb12c89ca
BudkaSnos3000 <icontab80@gmail.com>
2023-05-22 11:29:36 +0300

    1

--- WpfApp7/MainWindow.xaml.cs
+++ WpfApp7/MainWindow.xaml.cs
@@ -26,13 +26,13 @@ namespace AttachedProperties
                 return (Style)dObj.GetValue(AdditionalStyleProperty);
             }
 
-            public static void SetPlaceholderStyle(DependencyObject dObj, Style value)
+            public static void SetAdditionalStyle(DependencyObject dObj, Style value)
             {
                 dObj.SetValue(AdditionalStyleProperty, value);
             }
 
-            // Using a DependencyProperty as the backing store for PlaceholderStyle.  This enables animation, styling, binding, etc...
-            public static readonly DependencyProperty AdditionalStyleProperty =
+        // Using a DependencyProperty as the backing store for PlaceholderStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AdditionalStyleProperty =
                 DependencyProperty.RegisterAttached(nameof(GetAdditionalStyle)[3..], typeof(Style), typeof(Helper), new PropertyMetadata(null));
         }
    
